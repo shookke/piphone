bbf8195303675337db260d3f92f9b2346638790c
Kevin Shook <shookke@gmail.com>
2019-06-26 20:07:34 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import linphone
+import logging
+import sys
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication
+
+def main():
+	logging.basicConfig(level=logging.INFO)
+
+	app = QApplication(sys.argv)
+
+	def log_handler(level, msg):
+		method = getattr(logging, level)
+		method(msg)
+
+	def global_state_changed(*args, **kwargs):
+		logging.warning("global_state_changed: %r %r" % (args,kwargs))
+
+	def registration_state_changed(core, call, state, message):
+		logging.warning("registration_state_changed: " + str(state) + ", " + message)
+
+	callbacks = {
+		'global_state_changed': global_state_changed,
+		'registration_state_changed': registration_state_changed,
+	}
+
+	linphone.set_log_handler(log_handler)
+	core = linphone.Core.new(callbacks, None, None)
+	address = linphone.Address.new("sip:100@shookke.fl.3cx.us")
+	auth = linphone.AuthInfo.new(None,None,None,None,None,None)
+	auth.username = "100"
+	auth.passwd = "g2RfaXrwNg"
+	auth.userid = "zlE7Ln8bvD"
+	core.add_auth_info(auth)
+	#account.password = "3l4bgu0r0wmt"
+	#account.username = "000"
+	#account.test_validation()
+	proxy_cfg = core.create_proxy_config()
+	core.provisioning_uri = "https://shookke.fl.3cx.us/provisioning/wuvqph5halzuac4"
+	proxy_cfg.identity_address = address
+	proxy_cfg.server_addr = "sip:shookke.fl.3cx.us"
+	proxy_cfg.register_enabled = True
+	core.add_proxy_config(proxy_cfg)
+	core.invite("sip:000@shookke.fl.3cx.us")
+
+
+	iterate_timer = QTimer()
+	iterate_timer.timeout.connect(core.iterate)
+	stop_timer = QTimer()
+	stop_timer.timeout.connect(app.quit)
+	iterate_timer.start(20)
+	stop_timer.start(5000)
+
+	exitcode = app.exec_()
+	sys.exit(exitcode)
+
+main()
+
