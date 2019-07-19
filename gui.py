23ac380277dc2fdff7ecda229b3dcba8562a8ba8
Kevin <shookke@gmail.com>
2019-07-19 00:16:40 -0400

    .

--- gui.py
+++ gui.py
@@ -96,9 +96,9 @@ class Dialer(QWidget):
         self.num_bar.adjustSize()
         if self.call_state == 6:
             self.sip.start_dtmf_stream()
-            self.sip.play_dtmf(int(sender.text()), 50)
+            self.sip.play_dtmf(int(sender.text()), 100)
             print (sender.text())
-            self.sip.stop_dtmf_stream()
+            #self.sip.stop_dtmf_stream()
 
 
 
